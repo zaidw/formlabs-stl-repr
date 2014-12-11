827f4b4f2bc1328a86af45042c19ebf49c2331ec
Winta Zaid <zaidw@mit.edu>
2014-12-11 14:09:48 -0500

    First Commit

---
+++ STL_Representation/src/stlRepr/ReadIn.java
@@ -0,0 +1,14 @@
+package stlRepr;
+
+public class ReadIn {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//read in the file
+		//make an arrayList of triangles
+		//feed list into mesh object 
+	}
+
+}
---
+++ STL_Representation/src/stlRepr/Vertex.java
@@ -0,0 +1,35 @@
+package stlRepr;
+
+public class Vertex {
+
+	/**
+	 * @param args
+	 */
+	private float x;
+	private float y;
+	private float z;
+	
+	public Vertex(float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	
+	public float getX() {
+		return x;
+	}
+	
+	public float getY() {
+		return y;
+	}
+	
+	public float getZ() {
+		return z;
+	}
+	
+	// useful later?
+	public float distanceTo(Vertex v) {
+		return (float) 0.0;
+	}
+
+}
